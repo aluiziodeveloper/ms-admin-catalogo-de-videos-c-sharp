69c498b06276014eb7482a4f296e33284ae65b22
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-04-10 16:32:00 -0300

    Category validation rules

--- src/FC.Codeflix.Catalog.Domain/Entity/Category.cs
+++ src/FC.Codeflix.Catalog.Domain/Entity/Category.cs
@@ -19,12 +19,18 @@ public class Category
 
         Validate();
     }
-
+    
     private void Validate()
     {
         if (String.IsNullOrWhiteSpace(Name))
             throw new EntityValidationException($"{nameof(Name)} should not be empty or null");
+        if (Name.Length < 3)
+            throw new EntityValidationException($"{nameof(Name)} should be at leats 3 characters long");
+        if (Name.Length > 255)
+            throw new EntityValidationException($"{nameof(Name)} should be less or equal 255 characters long");
         if (Description == null)
             throw new EntityValidationException($"{nameof(Description)} should not be empty or null");
+        if (Description.Length > 10_000)
+            throw new EntityValidationException($"{nameof(Description)} should be less or equal 10.000 characters long");
     }
 }
