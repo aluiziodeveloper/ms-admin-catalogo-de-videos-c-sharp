669f977b3bd83e91febb3b545316eca9cd82fd80
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-04-10 20:15:35 -0300

    DomainValidation MinLength

--- tests/FC.Codeflix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -58,4 +58,51 @@ public class DomainValidationTest
 
         action.Should().NotThrow();
     }
+
+    [Theory(DisplayName = nameof(MinLengthThrowWhenLess))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    [MemberData(nameof(GetValuesSmallerThanMin), parameters: 10)]
+    public void MinLengthThrowWhenLess(string target, int minLength)
+    {
+        Action action =
+            () => DomainValidation.MinLength(target, minLength, "fieldName");
+
+        action.Should().Throw<EntityValidationException>()
+            .WithMessage($"fieldName should not be less than {minLength} characters long");
+    }
+
+    public static IEnumerable<object[]> GetValuesSmallerThanMin(int numberOftests = 5)
+    {
+        yield return new object[] { "123456", 10 };
+        var faker = new Faker();
+        for (int i = 0; i < (numberOftests - 1); i++)
+        {
+            var example = faker.Commerce.ProductName();
+            var minLength = example.Length + (new Random()).Next(1, 20);
+            yield return new object[] { example, minLength };
+        }
+    }
+
+    [Theory(DisplayName = nameof(MinLengthOk))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    [MemberData(nameof(GetValuesGreaterThanMin), parameters: 10)]
+    public void MinLengthOk(string target, int minLength)
+    {
+        Action action =
+            () => DomainValidation.MinLength(target, minLength, "fieldName");
+
+        action.Should().NotThrow();
+    }
+
+    public static IEnumerable<object[]> GetValuesGreaterThanMin(int numberOftests = 5)
+    {
+        yield return new object[] { "123456", 6 };
+        var faker = new Faker();
+        for (int i = 0; i < (numberOftests - 1); i++)
+        {
+            var example = faker.Commerce.ProductName();
+            var minLength = example.Length - (new Random()).Next(1, 5);
+            yield return new object[] { example, minLength };
+        }
+    }
 }
