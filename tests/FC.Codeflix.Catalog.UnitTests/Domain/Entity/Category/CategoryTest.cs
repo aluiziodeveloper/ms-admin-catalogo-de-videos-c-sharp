32b75b1513916e04ab54514d86f3d7a275e46b8f
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-04-11 20:56:35 -0300

    Adjusts

--- tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -96,7 +96,7 @@ public class CategoryTest
 
         action.Should()
             .Throw<EntityValidationException>()
-            .WithMessage("Name should be at leats 3 characters long");
+            .WithMessage("Name should be at least 3 characters long");
     }
 
     public static IEnumerable<object[]> GetNamesWithLessThan3Characters(int numberOfTests = 6)
@@ -222,7 +222,7 @@ public class CategoryTest
 
         action.Should()
             .Throw<EntityValidationException>()
-            .WithMessage("Name should be at leats 3 characters long");
+            .WithMessage("Name should be at least 3 characters long");
     }
 
     [Fact(DisplayName = nameof(UpdateErrorWhenNameIsGreaterThan255Characters))]
