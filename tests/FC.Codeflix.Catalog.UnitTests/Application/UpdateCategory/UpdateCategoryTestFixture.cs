ede58fa22bfc034d4cffae28977df9402855b77a
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-04-13 17:44:01 -0300

    UpdateCategory exceptions tests

--- tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTestFixture.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTestFixture.cs
@@ -58,4 +58,32 @@ public class UpdateCategoryTestFixture
             GetValidCategoryDescription(),
             getRandomBoolean()
         );
+
+    public UpdateCategoryInput GetInvalidInputShortName()
+    {
+        var invalidInputShortName = GetValidInput();
+        invalidInputShortName.Name =
+            invalidInputShortName.Name.Substring(0, 2);
+        return invalidInputShortName;
+    }
+
+    public UpdateCategoryInput GetInvalidInputTooLongName()
+    {
+        var invalidInputTooLongName = GetValidInput();
+        var tooLongNameForCategory = Faker.Commerce.ProductName();
+        while (tooLongNameForCategory.Length <= 255)
+            tooLongNameForCategory = $"{tooLongNameForCategory} {Faker.Commerce.ProductName()}";
+        invalidInputTooLongName.Name = tooLongNameForCategory;
+        return invalidInputTooLongName;
+    }
+
+    public UpdateCategoryInput GetInvalidInputTooLongDescription()
+    {
+        var invalidInputTooLongDescription = GetValidInput();
+        var tooLongDescriptionForCategory = Faker.Commerce.ProductDescription();
+        while (tooLongDescriptionForCategory.Length <= 10_000)
+            tooLongDescriptionForCategory = $"{tooLongDescriptionForCategory} {Faker.Commerce.ProductDescription()}";
+        invalidInputTooLongDescription.Description = tooLongDescriptionForCategory;
+        return invalidInputTooLongDescription;
+    }
 }
