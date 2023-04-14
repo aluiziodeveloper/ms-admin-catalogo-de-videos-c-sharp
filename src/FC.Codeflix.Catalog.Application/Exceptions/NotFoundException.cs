d8ff90f222d940a150fd7a61ce5625e07e112489
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-04-14 11:46:55 -0300

    CategoryRepository - get method tests

--- src/FC.Codeflix.Catalog.Application/Exceptions/NotFoundException.cs
+++ src/FC.Codeflix.Catalog.Application/Exceptions/NotFoundException.cs
@@ -2,4 +2,12 @@
 public class NotFoundException : ApplicationException
 {
     public NotFoundException(string? message) : base(message) { }
+
+    public static void ThrowIfNull(
+        object? @object,
+        string exceptionMessage)
+    {
+        if (@object == null)
+            throw new NotFoundException(exceptionMessage);
+    }
 }
--- src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
@@ -1,12 +1,12 @@
-﻿using FC.Codeflix.Catalog.Domain.Entity;
+﻿using FC.Codeflix.Catalog.Application.Exceptions;
+using FC.Codeflix.Catalog.Domain.Entity;
 using FC.Codeflix.Catalog.Domain.Repository;
 using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
 using Microsoft.EntityFrameworkCore;
 
 namespace FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
 
-public class CategoryRepository
-    : ICategoryRepository
+public class CategoryRepository : ICategoryRepository
 {
     private readonly CodeflixCatalogDbContext _context;
     private DbSet<Category> _categories
@@ -22,11 +22,14 @@ public class CategoryRepository
         => await _categories.AddAsync(aggregate, cancellationToken);
 
     public async Task<Category> Get(Guid id, CancellationToken cancellationToken)
-        => await _categories.FindAsync(
+    {
+        var category = await _categories.FindAsync(
             new object[] { id },
             cancellationToken
         );
-
+        NotFoundException.ThrowIfNull(category, $"Catgory '{id}' not found.");
+        return category!;
+    }
 
     public Task Delete(Category aggregate, CancellationToken cancellationToken) => throw new NotImplementedException();
     public Task<SearchOutput<Category>> Search(SearchInput input, CancellationToken cancellationToken) => throw new NotImplementedException();
