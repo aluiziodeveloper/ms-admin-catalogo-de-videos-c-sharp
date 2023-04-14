588cf7466791284365f43db1889f41566d3cd6c6
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-04-14 09:45:17 -0300

    CategoryRepository - insert method - part 1

--- tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
@@ -1,4 +1,6 @@
-﻿using FC.Codeflix.Catalog.IntegrationTests.Base;
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.IntegrationTests.Base;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository;
@@ -8,4 +10,44 @@ public class CategoryRepositoryTestFixtureCollection
     : ICollectionFixture<CategoryRepositoryTestFixture> {}
 
 public class CategoryRepositoryTestFixture
-    : BaseFixture {}
+    : BaseFixture
+{
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription =
+            Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10_000)
+            categoryDescription =
+                categoryDescription[..10_000];
+        return categoryDescription;
+    }
+
+    public bool getRandomBoolean()
+        => new Random().NextDouble() < 0.5;
+
+    public Category GetExampleCategory()
+        => new(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            getRandomBoolean()
+        );
+
+    public CodeflixCatalogDbContext CreateDbContext()
+    {
+        var dbContext = new CodeflixCatalogDbContext(
+            new DbContextOptionsBuilder<CodeflixCatalogDbContext>()
+            .UseInMemoryDatabase("integration-tests-db")
+            .Options
+        );
+    }
+}
